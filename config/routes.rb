ede9ecff66a3260a8adae0996d94b2bba0ee50f6
pierreq <pierre@eventtia.com>
2022-10-01 16:25:38 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
